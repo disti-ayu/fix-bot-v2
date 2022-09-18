eec7b1f8e2b3837a9426c8cd1de6519394b2a104
disti-ayu <113271848+disti-ayu@users.noreply.github.com>
2022-09-18 13:17:31 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,17 +1,18 @@
 let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
 let text = `
 ┌─「 Donasi • Pulsa 」
-│ • *Telkomsel:* [081225377760]
+│ • *Telkomsel:* [${global.ppulsa}]
 ❏────
 
 ┌─「 Donasi • Non Pulsa 」
 │ • *Dana:* [${global.pdana}]
 │ • *Gopay:* [${global.pgopay}]
 │ • *Ovo:* [${global.povo}]
+│ • *Qris All Pay:* https://i.ibb.co/T2DPGvq/donasi.png
 │ • *Link Aja:* [${global.plinkaja}]
 ❏────
 `.trim()
-  m.reply(txt)
+  m.reply(text)
 }
 handler.help = ['donasi']
 handler.tags = ['info']
